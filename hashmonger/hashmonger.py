d19bb3a5692ee9bfaf5fcec49451ab7de10a6377
Eric Kates <erickates@BigMac.local>
2023-02-18 09:40:03 -0800

    added some error handling

--- hashmonger/hashmonger.py
+++ hashmonger/hashmonger.py
@@ -9,7 +9,7 @@ import io
 from typing import Optional
 from rich import print
 
-__version__ = "0.2.0"
+__version__ = "0.2.1"
 
 def version_callback(value: Optional[bool]):
     if value:
@@ -19,7 +19,6 @@ def version_callback(value: Optional[bool]):
 def get_item_type(item_path: str) -> Optional[str]:
     """
     Checks if the supplied path is to a real folder or file. 
-    If not valid, program exits. Only works for files at this time.
     """
    
     if os.path.exists(item_path):
@@ -28,8 +27,9 @@ def get_item_type(item_path: str) -> Optional[str]:
         elif os.path.isdir(item_path):
             return "dir"
         else:
-            raise typer.Exit()
-    raise typer.Exit()
+            return None
+  
+    raise ValueError
 
 def get_hashes(item_path: str) -> dict:
     BUFFER = io.DEFAULT_BUFFER_SIZE
@@ -60,13 +60,26 @@ def get_hashes(item_path: str) -> dict:
 
     return hash_dict
 
-def main(item_path: str = typer.Option(..., "--path", "-p", help="File or folder path to be hashed", prompt="Path (file or folder)", show_default=False),
-         hash_type: str = typer.Option(None, "--hash", "-d", help="Specify a single hash type. Default: return all available hash types.", show_default=False),
-         version: Optional[bool] = typer.Option(None, "--version", callback=version_callback, is_eager=True)
+def main(item_path: str = typer.Option(
+            ..., "--path", "-p", help="File or folder path to be hashed", 
+            prompt="Path (file or folder)", show_default=False
+            ),
+         hash_type: str = typer.Option(
+            None, "--hash", "-d", 
+            help="Specify a single hash type. Default: return all available hash types.", 
+            show_default=False
+            ),
+         version: Optional[bool] = typer.Option(
+            None, "--version", 
+            callback=version_callback, is_eager=True
+            )
 ):
     """Compute file/folder hashes."""
-
-    item_type = get_item_type(item_path)
+    try:
+        item_type = get_item_type(item_path)
+    except ValueError:
+        print("The path you provided is not valid.\nEnd of line.")
+        raise typer.Exit()
 
     if item_type == "file":
         hashes = get_hashes(item_path)
