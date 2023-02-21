59c8bd2ba534e09dc0287a7ccbf8dd003b8c3f53
Eric Kates <erickates@BigMac.local>
2023-02-21 11:22:06 -0800

    MVP of OOP refactor

--- hashmonger/hasher.py
+++ hashmonger/hasher.py
@@ -1,14 +1,11 @@
 """Main entrypoint for Hashmonger script"""
 # hashmonger/main.py
 
-import os.path
-import typer
 import hashlib
 import io
-
 from pathlib import Path
 from typing import Optional
-from hashmonger import __app_name__, __version__
+
 
 class Hasher:
     """
@@ -18,15 +15,33 @@ class Hasher:
     """
 
     def __init__(self, path: str):
-        self.file_path = path
+        self.item_path = self._validate_path(path)
+        self.item_type = self._get_path_type(self.item_path)
+        self.buffer_size = io.DEFAULT_BUFFER_SIZE
+        
+        _sha1_hash = hashlib.sha1()
+        _sha256_hash = hashlib.sha256()
+        _md5_hash = hashlib.md5()
 
-    def get_path_type(self) -> Optional[str]:
+        self.hashes = {
+            "SHA256": _sha256_hash,
+            "SHA1": _sha1_hash,
+            "MD5": _md5_hash
+        }
+
+    def _validate_path(self, item_path: str) -> str:
+        if Path(item_path).exists():
+            return item_path
+        else:
+            raise FileNotFoundError("The path provided does not exist.")
+        
+    def _get_path_type(self, item_path: str) -> Optional[str]:
         """
         Validates a given file/folder path and returns whether
         the path leads to a file or folder. 
         """
-        path_is_file = Path.is_file(self.file_path)
-        path_is_dir = Path.is_dir(self.file_path)
+        path_is_file = Path(item_path).is_file()
+        path_is_dir = Path(item_path).is_dir()
 
         if path_is_file:
             return "File"
@@ -35,48 +50,25 @@ class Hasher:
         else:
             return None
 
+    def list_hash_types(self, hashes: dict):
+        print("The following hash types are currently supported:\n")
+        for hashtype in hashes:
+            print(f"\n{hashtype}")
 
-def get_hashes(item_path: str) -> dict:
-    BUFFER = io.DEFAULT_BUFFER_SIZE
-    sha1 = hashlib.sha1()
-    sha256 = hashlib.sha256()
-    md5 = hashlib.md5()
-
-    hash_dict = {
-        "SHA256": sha256,
-        "SHA1": sha1,
-        "MD5": md5
-    }
-
-    # if we're hashing a file open it and begin reading bytes into the hash objects
-    with open(item_path, "rb") as binary_reader:
-        # update all hash opbjects in the hash dictionary with bytes read from file
-        while True:
-            read_buffer = binary_reader.read(BUFFER)
-
-            if len(read_buffer) == 0:
-                break
-            
-            for hash_type in hash_dict:
-                hash_dict[hash_type].update(read_buffer)
-
-        # update read_buffer with how many bytes were read
-        bytes_read = len(read_buffer)
-
-    return hash_dict
-
-    """Compute file/folder hashes."""
-    try:
-        item_type = get_item_type(item_path)
-    except ValueError:
-        print("The path you provided is not valid.\nEnd of line.")
-        raise typer.Exit()
-
-    if item_type == "file":
-        hashes = get_hashes(item_path)
-        for hash in hashes:
-            print(f"{hash}\t\t{hashes[hash].hexdigest()}")
-    elif item_type == "dir":
-        print("Directory")
+    def get_hashes(self) -> dict:
+        """
+        Computes hashes for given has type and item path.
+        This is a 'private' method and expects a vetted item path.
+        """
+        with open(self.item_path, "rb") as binary_reader:
+            while True:
+                read_buffer = binary_reader.read(self.buffer_size)
+                if len(read_buffer) == 0:
+                    break
+                # update all hash objects in the hash dictionary 
+                # with bytes read from file
+                for hash_type in self.hashes:
+                    self.hashes[hash_type].update(read_buffer)
+        return self.hashes
 
 
